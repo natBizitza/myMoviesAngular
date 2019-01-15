1cd17fc94f4f3a50888c8e68052078ad7a62b2f2
Natalia Romankevich <romankevich.law@gmail.com>
2019-01-15 18:56:08 +0100

    lastviewedIsHidden

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Movie } from '../movie';
 import { MovieService } from '../movie.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.css']
 })
+
 export class MoviesComponent implements OnInit {
-  p: number = 1;  
+  p: number = 1;
+  currentPage: number;
+  totalItems: number;  
   movies: Movie[];
 
-  constructor(private movieService: MovieService) { }
+  constructor(private movieService: MovieService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.getMovies();
